d120966b7b01de802f106fb3570b3e9f660b632c
Nitin Kushwaha <79303072+Nitin13-git@users.noreply.github.com>
2024-09-25 08:19:50 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -3,7 +3,7 @@ from huggingface_hub import login
 import os
 
 
-os.environ["HF_API_TOKEN"] = "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"
+os.environ["HF_API_TOKEN"] = "Your HF Token"
 login(token=os.environ["HF_API_TOKEN"])
 dataset = load_dataset("imagefolder", data_dir="E:/Nitin_sitare_stable_diffusion/images", drop_labels=True)
 dataset.push_to_hub("Nitin9sitare/Swami_vivekananda_DreamBoth_dataset")

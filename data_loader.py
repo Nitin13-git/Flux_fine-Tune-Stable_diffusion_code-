39a99ce3908ecee3a12cde8160e757911730dc18
Nitin Kushwaha <79303072+Nitin13-git@users.noreply.github.com>
2024-09-25 08:30:40 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -6,4 +6,4 @@ import os
 os.environ["HF_API_TOKEN"] = "Your HF Token"
 login(token=os.environ["HF_API_TOKEN"])
 dataset = load_dataset("imagefolder", data_dir="E:/Nitin_sitare_stable_diffusion/images", drop_labels=True)
-dataset.push_to_hub("Nitin9sitare/Swami_vivekananda_DreamBoth_dataset")
+dataset.push_to_hub("Nitin9sitare/stable_diffusion_Swami_Vivekananda")

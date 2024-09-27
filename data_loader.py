97ee5f5b594ba0d2a4ff2e25d07377d9a3bcbd7b
Nitin Kushwaha <79303072+Nitin13-git@users.noreply.github.com>
2024-09-27 21:48:14 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -3,8 +3,16 @@ from huggingface_hub import login
 import os
 
 
+# Set your Hugging Face API token as an environment variable
 os.environ["HF_API_TOKEN"] = "Your HF Token"
+
+# Log in to Hugging Face using the token stored in the environment variable
 login(token=os.environ["HF_API_TOKEN"])
-dataset = load_dataset("imagefolder", data_dir="E:/Nitin_sitare_stable_diffusion/images", drop_labels=True)
+
+# Load the dataset from a specified directory (replace "you data dir" with your actual directory path)
+dataset = load_dataset("imagefolder", data_dir="you data dir", drop_labels=True)
+
+# Push your dataset to the Hugging Face Hub using your dataset ID
 dataset.push_to_hub("Your HF dataset ID")
-# dataset.push_to_hub("Nitin9sitare/stable_diffusion_Swami_Vivekananda")
+# Example: dataset.push_to_hub("Nitin9sitare/stable_diffusion_Swami_Vivekananda")
+

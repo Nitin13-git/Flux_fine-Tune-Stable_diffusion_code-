6055844ea9eae35cdb539072b299f3335e345b3d
Nitin Kushwaha <79303072+Nitin13-git@users.noreply.github.com>
2024-09-27 21:08:57 +0530

    Update fal_inference.py

--- fal_inference.py
+++ fal_inference.py
@@ -6,7 +6,14 @@ handler = fal_client.submit(
     "fal-ai/lora",
     arguments={
         "model_name": "stabilityai/stable-diffusion-xl-base-1.0",
-        # you can repliace with your finetune model name
+        # Replace 'correctwy--' with the name of your fine-tuned model.
+        # To update the model name:
+        # 1. Go to Fal.ai's official website: https://fal.ai/
+        # 2. Log in using your authenticated Git credentials.
+        # 3. Click on the "Home" icon.
+        # 4. You will see your fine-tuned model listed.
+        # 5. Click on "Playground" to access the model.
+        # 6. Copy the model name and update it in the script.
         "prompt": "photo of a rhino dressed suit and tie sitting at a table in a bar with a bar stools, award winning photography, Elke vogelsang"
     },
 )
